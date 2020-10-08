f766afe25f35e7ea8971104a3bb45e79cd15c82d
amencel <amencel@hawaii.edu>
2020-10-08 10:31:53 -1000

    stuff

---
+++ quizzes/10.js
@@ -0,0 +1,5 @@
+var student= ['ITM352', 'BUS311', 'ACC101', 'FIN100', 'BUS310', 'MOM202'];
+student[2]= 'ITM353';
+console.log(student[2]);
+student.push('MKT300');
+console.log(student);
---
+++ quizzes/pt2.js
@@ -0,0 +1,5 @@
+student=  {firstName: 'Dan', lastName: 'Kazman', major: 'MIS', year: 3, id: 123456789, onProbation: true};
+console.log(student.major);
+student.onProbation= false;
+student.clubMembership= 'ITMA';
+console.log(student);
